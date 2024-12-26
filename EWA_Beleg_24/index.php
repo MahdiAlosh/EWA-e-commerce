c7e15b559c2cfa169fa564c23734585a223fafa4
Ali Al Mahdi <alimhdy777@gmail.com>
2024-12-26 16:03:56 +0100

    uploading new logo & PopUp-Korb resizing

--- EWA_Beleg_24/index.php
+++ EWA_Beleg_24/index.php
@@ -3,31 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="theme-color" content="#ffffff">
         <meta name="description" content="willkommen im G05-Laden">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="./assets/img/log/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
 
         <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-        <!-- <link rel="stylesheet" type="text/css" href="./assets/css/style.css"> -->
         
         <link rel="icon" href="./assets/img/logo/favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="57x57" href="./assets/img/logo/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="./assets/img/logo/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="./assets/img/logo/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/logo/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="./assets/img/logo/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="./assets/img/logo/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="./assets/img/logo/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="./assets/img/logo/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/logo/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="./assets/img/logo/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/logo/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="./assets/img/logo/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/logo/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="./assets/img/logo/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="./assets/img/logo/ms-icon-144x144.png">
+        
 
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
         <script src="https://unpkg.com/vue-router@4"></script>
@@ -39,7 +39,7 @@
                 <div class="d-flex px-2 py-3 container">
                     <div>
                         <router-link to="/">
-                            <img class="mainlogo" src="./assets/img/logo/apple-icon-57x57.png" alt="g09">
+                            <img class="mainlogo" src="./assets/img/logo/ms-icon-70x70.png" alt="g05">
                         </router-link>
                     </div>
                     <div class="d-flex align-items-center w-100 ms-2">
