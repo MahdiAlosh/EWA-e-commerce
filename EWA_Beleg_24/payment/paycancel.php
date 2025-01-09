cd738293fb90f54efd3bc7264255135d164ecaf0
Abdallah Alherani <s85758@htw-dresden.de>
2025-01-09 21:27:46 +0100

    payment done

--- EWA_Beleg_24/payment/paycancel.php
+++ EWA_Beleg_24/payment/paycancel.php
@@ -1,22 +1,33 @@
 <?php
-    $session = $_GET['session_id'];
-    $bodyres='<h1>Canceled</h1><span>Canceled '. $session .'</span>';
+// Überprüfen, ob session_id gesetzt ist
+$session = $_GET['session_id'] ?? null;
 
-    $body= '
-    <div class="container mt-3">
-        <div>
-            '. $bodyres .'
-        </div>
-        <div class="d-flex justify-content-center">
-            <button class="btn btn-primary w-50 mt-3 mb-4" onclick="reloadWithoutGetParams()">Back</button>
-        </div>
+if ($session) {
+    $bodyres = '<h1>Zahlung abgebrochen</h1>
+                <p>Ihre Zahlung mit der Sitzungs-ID <strong>' . htmlspecialchars($session) . '</strong> wurde abgebrochen.</p>';
+} else {
+    $bodyres = '<h1>Fehler!</h1><p>Es konnte keine gültige Sitzungs-ID gefunden werden.</p>';
+}
+
+// HTML-Ausgabe
+$body = '
+<div class="container mt-3">
+    <div>
+        ' . $bodyres . '
     </div>
+    <div class="d-flex justify-content-center">
+        <a href="/EWA_BELEG_24/" class="btn btn-primary w-50 mt-3 mb-4">Zurück zur Startseite</a>
+        <a href="/EWA_BELEG_24/korb.php" class="btn btn-secondary w-50 mt-3 mb-4 ms-2">Zurück zum Warenkorb</a>
+    </div>
+</div>
 
-    <script>
-        function reloadWithoutGetParams() {
-            var urlWithoutParams = window.location.origin + window.location.pathname;
-            window.location.href = urlWithoutParams;
-        }
-    </script>
-    ';
+<script>
+    function reloadWithoutGetParams() {
+        var urlWithoutParams = window.location.origin + window.location.pathname;
+        window.location.href = urlWithoutParams;
+    }
+</script>
+';
+
+echo $body;
 ?>
